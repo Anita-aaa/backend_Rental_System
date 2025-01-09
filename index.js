1345eba526eafc55f84ac8831de15732be03b9b4
Anita <at9941787@gmail.com>
2025-01-09 13:57:36 +0545

    Mongodb connect

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ const errorHandler = require('./middleware/errorHandler');
 const User = require('./models/user');
 const swagger = require('./swagger');
 const cors = require('cors')
-
-
-
 connectDB();
 const app = express();
 const port = process.env.PORT || 3000;
