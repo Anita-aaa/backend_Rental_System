b66de404b059456b2d9b0ce3ae5d7f5392e22da7
Anita <at9941787@gmail.com>
2025-03-03 14:01:44 +0545

    Mongodb connect

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-// Connect to the database
+
+
 const connectDB = async () => { 
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
